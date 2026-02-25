bb44e976556ae9e89a17fe6c6d24c6ab8026f818
Igor Ryan <igorryanmendesalves@gmail.com>
2026-02-25 19:46:42 -0300

    Iniciando controller de usuários

--- src/main/java/com/stcakyforge/matchpoint/controller/UsuarioController.java
+++ src/main/java/com/stcakyforge/matchpoint/controller/UsuarioController.java
@@ -1,4 +1,46 @@
 package com.stcakyforge.matchpoint.controller;
 
+import com.stcakyforge.matchpoint.dtos.request.UsuarioRequestDto;
+import com.stcakyforge.matchpoint.dtos.response.UsuarioResponseDto;
+import com.stcakyforge.matchpoint.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDto> criarUsuario(@RequestBody UsuarioRequestDto usuarioRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.salvarUsuario(usuarioRequestDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDto>> pegarUsuarios() {
+        return ResponseEntity.ok(usuarioService.listaUsuarios());
+    }
+
+    @GetMapping("/{id}")
+    public UsuarioResponseDto pegarUsuario(@PathVariable Long id) {
+        return usuarioService.pegarUsuarioPorId(id);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDto> atualizarUsuario(@PathVariable Long id, @RequestBody UsuarioRequestDto usuarioRequestDto) {
+        return ResponseEntity.ok(usuarioService.salvarUsuario(usuarioRequestDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+        usuarioService.deleteUsuario(id);
+        return  ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
