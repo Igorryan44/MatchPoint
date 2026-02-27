1782ace0afa1fb7ce2bd57bdf8c32ac283c26b00
Igor Ryan <igorryanmendesalves@gmail.com>
2026-02-26 22:34:50 -0300

    Ajuste no .gitignore

--- src/main/java/com/stcakyforge/matchpoint/service/JogadorService.java
+++ src/main/java/com/stcakyforge/matchpoint/service/JogadorService.java
@@ -1,4 +1,14 @@
 package com.stcakyforge.matchpoint.service;
 
+import com.stcakyforge.matchpoint.repository.JogadorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JogadorService {
+
+    private final JogadorRepository jogadorRepository;
+
+    public JogadorService(JogadorRepository jogadorRepository) {
+        this.jogadorRepository = jogadorRepository;
+    }
 }
