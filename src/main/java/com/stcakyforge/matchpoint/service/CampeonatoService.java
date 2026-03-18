140cabd0be4a9de182e1714370f59cbad7a62350
Igor Ryan <igorryanmendesalves@gmail.com>
2026-03-18 20:03:46 -0300

    feat/Crud de campeonato

--- src/main/java/com/stcakyforge/matchpoint/service/CampeonatoService.java
+++ src/main/java/com/stcakyforge/matchpoint/service/CampeonatoService.java
@@ -2,23 +2,64 @@ package com.stcakyforge.matchpoint.service;
 
 import com.stcakyforge.matchpoint.dtos.request.CampeonatoRequestDto;
 import com.stcakyforge.matchpoint.dtos.response.CampeonatoResponseDto;
+import com.stcakyforge.matchpoint.dtos.response.JogadorResponseDto;
 import com.stcakyforge.matchpoint.mapper.CampeonatoMapper;
+import com.stcakyforge.matchpoint.mapper.JogadorMapper;
+import com.stcakyforge.matchpoint.model.Campeonato;
+import com.stcakyforge.matchpoint.model.Jogador;
 import com.stcakyforge.matchpoint.repository.CampeonatoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 public class CampeonatoService {
 
-    private  final CampeonatoRepository campeonatoRepository;
-    private  final CampeonatoMapper mapper;
+    private final CampeonatoRepository campeonatoRepository;
+    private final CampeonatoMapper mapper;
+    private final JogadorMapper jogadorMapper;
 
-    public CampeonatoService(CampeonatoRepository campeonatoRepository, CampeonatoMapper mapper) {
+    public CampeonatoService(CampeonatoRepository campeonatoRepository, CampeonatoMapper mapper, JogadorMapper jogadorMapper) {
         this.campeonatoRepository = campeonatoRepository;
         this.mapper = mapper;
+        this.jogadorMapper = jogadorMapper;
     }
 
     public ResponseEntity<CampeonatoResponseDto> criarCampeonato(CampeonatoRequestDto campeonatoRequestDto) {
         return ResponseEntity.ok(mapper.toDto(campeonatoRepository.save(mapper.toEntity(campeonatoRequestDto))));
     }
+
+    public ResponseEntity<Void> deletarCampeonato(Long id) {
+        campeonatoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    public ResponseEntity<List<CampeonatoResponseDto>> listarCampeopnatos() {
+        return ResponseEntity.ok(mapper.toDto(campeonatoRepository.findAll()));
+    }
+
+    public ResponseEntity<List<JogadorResponseDto>> rankingJogadoresPorCampeonato(Long campeonatoId) {
+
+        Campeonato campeonato = campeonatoRepository.findById(campeonatoId).orElseThrow(() -> new EntityNotFoundException("Campeonato não encontrado"));
+
+        List<Jogador> ranking = campeonato.getJogadores().stream()
+                .sorted(Comparator.comparingInt(Jogador::getPontos).reversed())
+                .toList();
+
+        return ResponseEntity.ok(jogadorMapper.toDto(ranking));
+    }
+
+    public ResponseEntity<List<JogadorResponseDto>> rankingJogadoresGeral() {
+
+        List<Jogador> ranking = campeonatoRepository.findAll()
+                .stream().flatMap(c -> c.getJogadores().stream())
+                .sorted(Comparator.comparingInt(Jogador::getPontos).reversed())
+                .toList();
+
+        return ResponseEntity.ok(jogadorMapper.toDto(ranking));
+    }
+
 }
